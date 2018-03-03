5c30fbd90522c9a307beade37d8075c2be829bea
leogtzr <leogutierrezramirez@gmail.com>
2018-03-03 16:19:48 -0400

    Adding IT test

--- src/main/java/toomuchdrinking/App.java
+++ src/main/java/toomuchdrinking/App.java
@@ -2,7 +2,6 @@ package toomuchdrinking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"toomuchdrinking.repository", "toomuchdrinking.controller"})
 public class App {
