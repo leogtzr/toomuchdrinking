280f87f205c2b8bc7999baa8638b3c8e64a5976d
leogtzr <leogutierrezramirez@gmail.com>
2018-03-08 00:08:25 -0400

    Adding postgres DB

--- src/main/java/toomuchdrinking/config/AppConfig.java
+++ src/main/java/toomuchdrinking/config/AppConfig.java
@@ -1,8 +1,19 @@
 package toomuchdrinking.config;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
-
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
