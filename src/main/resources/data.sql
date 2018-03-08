35f9709adbd6167e4ee740f16fed7c869b58c0f4
leogtzr <leogutierrezramirez@gmail.com>
2018-03-08 00:50:40 -0400

    Removing inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO drink_type(type) VALUES('Beer');
-INSERT INTO drink_type(type) VALUES('Whiskey');
+-- INSERT INTO drink_type(type) VALUES('Beer');
+-- INSERT INTO drink_type(type) VALUES('Whiskey');
 
 --INSERT INTO drink(abv,description,drink_date,milliliters,quantity,drink_type_id)
 --VALUES(1.1, 'Test desc 1', '2018-03-01', 10, 1, 1);
