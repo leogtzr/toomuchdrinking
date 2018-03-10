40b6068c31b019f3e0124e81a782dd028b7e5b73
leogtzr <leogutierrezramirez@gmail.com>
2018-03-09 23:00:25 -0400

    Adding more for stats

--- src/main/java/toomuchdrinking/controller/StatsController.java
+++ src/main/java/toomuchdrinking/controller/StatsController.java
@@ -1,13 +1,20 @@
 package toomuchdrinking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import toomuchdrinking.service.DrinkStatsService;
 
 @Controller
 public class StatsController {
 
+    @Autowired
+    private DrinkStatsService service;
+
     @RequestMapping("/stats")
-    public String welcome() {
+    public String stats(final Model model) {
+        model.addAttribute("max", service.maximums());
         return "stats";
     }
 
